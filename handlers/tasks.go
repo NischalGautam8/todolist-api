8113f890a383f9aa8b9810df4ebb563e5e5d7eec
headshigh <nischalgautam7200@gmail.com>
2025-03-20 20:29:46 +0545

    completed task handler

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -25,3 +25,37 @@ func GetTasks(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, tasks)
 }
+
+func CreateTask(c *gin.Context) {
+	var task models.Task
+	if err := c.BindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	_, err := db.DB.Exec("INSERT INTO task (title, desce, completed) VALUES ($1, $2, $3)", task.Title, task.Desce, task.Completed)
+	if err != nil {
+		panic(err.Error())
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Task created successfully"})
+}
+func UpdateTask(c *gin.Context) {
+	var task models.Task
+	if err := c.BindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	_, err := db.DB.Exec("UPDATE task SET title = $1, desce = $2, completed = $3 WHERE id = $4", task.Title, task.Desce, task.Completed, task.Id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+}
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+	_, err := db.DB.Exec("delete from task where id = $1", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(http.StatusNoContent)
+}
